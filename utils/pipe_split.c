20b63dd9563db52554ea80bfcadbed7460572d09
Mohamed-Amine Izem <mizem@c1r4p9.1337.ma>
2024-07-30 22:56:05 +0100

    f

--- utils/pipe_split.c
+++ utils/pipe_split.c
@@ -6,25 +6,48 @@
 /*   By: mizem <mizem@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 12:29:36 by mizem             #+#    #+#             */
-/*   Updated: 2024/07/27 22:50:54 by mizem            ###   ########.fr       */
+/*   Updated: 2024/07/30 22:55:38 by mizem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-void	ft_strncpy(char *s1, char *s2, int len)
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_split.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mizem <mizem@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/20 12:29:36 by mizem             #+#    #+#             */
+/*   Updated: 2024/07/29 21:43:24 by mizem            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+void *ft_free(char **str)
 {
-	int i = 0;
-	while (s2[i] && i < len)
-	{
-		s1[i] = s2[i];
-		i++;
-	}
-	s1[i] = '\0';
+    int i = 0;
+    while (str[i])
+    {
+        free(str[i]);
+        i++;
+    }
+    free(str);
+    return NULL;
+}
+void ft_strncpy(char *s1, char *s2, int len)
+{
+    int i = 0;
+    while (s2[i] && i < len) {
+        s1[i] = s2[i];
+        i++;
+    }
+    s1[i] = '\0';
 }
 
-int count_wc(char *str, char c)
-{
+int count_wc(char *str, char c) {
     int i = 0;
     int wc = 0;
     int flag = 0;
@@ -36,87 +59,75 @@ int count_wc(char *str, char c)
     {
         if (str[i] == '"')
             flag = !flag;
-        if (flag == 0 && str[i] == c)
-		{
+        if (!flag && str[i] == c)
+        {
             while (str[i] && str[i] == c)
                 i++;
         }
-		else if (flag == 0)
-		{
-            while (str[i] && str[i] != c)
-			{
-				if (str[i] == '"')
-					break;
+        else
+        {
+            wc++;
+            while (str[i] && (flag || str[i] != c))
+            {
+                if (str[i] == '"')
+                    flag = !flag;
                 i++;
-			}
-			wc++;
+            }
         }
-		else
-            i++;
-	}
+    }
     return wc;
 }
 
 
-char **ft_split(char *str, char c)
-{
-	int i;
-	int j;
-	int start;
-	int end;
-	char **out;
+char **pipe_split(char *str, char c) {
+    int i, j, start, end, wc;
+    char **out;
+    int flag = 0;
 
-	i = 0;
-	j = 0;
-	start = 0;
-	end = 0;
-	if (!str)
-		return NULL;
-	int wc = count_wc(str, c);
-	out = malloc(sizeof(char *) * (wc + 1));
-	if (!out)
-		return NULL;
-	while (str[i])
-	{
-		while (str[i] && str[i] == c)
-			i++;
-		if (str[i] == '"')
+    i = 0;
+    j = 0;
+    if (!str)
+        return NULL;
+
+    wc = count_wc(str, c);
+    out = malloc(sizeof(char *) * (wc + 1));
+    if (!out)
+        return NULL;
+
+    while (str[i])
+    {
+        while (str[i] && str[i] == c)
+            i++;
+        start = i;
+        while (str[i])
         {
-            start = i++;
-            while (str[i] && str[i] != '"')
-                i++;
-            end = ++i;
+            if (str[i] == '"')
+                flag = !flag;
+            else if (str[i] == c && !flag)
+                break;
+            i++;
         }
-		else
-		{
-			start = i;
-			while (str[i] && str[i] != c)
-			{
-				i++;
-				if (str[i] == '"')
-					break;
-			}
-			end = i;
-		}
-        while (end > start && str[end - 1] == ' ')
-            end--;
-		if (start < end)
-		{
-			out[j] = malloc(sizeof(char) * ((end - start) + 1));
-			if (!out)
-				return NULL;
-			ft_strncpy(out[j], &str[start], (end - start));
-			j++;
-		}
-	}
-	out[j] = NULL;
-	return out;
+        end = i;
+        // while (end > start && str[end - 1] == ' ')
+        //     end--;
+        if (start < end)
+        {
+            out[j] = malloc(sizeof(char) * ((end - start) + 1));
+            if (!out[j])
+                ft_free(out);
+            ft_strncpy(out[j], &str[start], (end - start));
+            j++;
+        }
+        // if (str[i] == c)
+        //     i++;
+    }
+    out[j] = NULL;
+    return out;
 }
 
 int main()
 {
-	printf("<<%d>>\n", count_wc("please \"work | now   \"", '|'));
-	char **str = ft_split("please \"work | now   \"", '|');
+	char **str = pipe_split("  please   \"work | now   \" ||  did it work   ", ' ');
 	int i = 0;
 	while (str[i])
 	{
