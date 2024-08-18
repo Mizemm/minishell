c21f9019e043ba6a78226224d6f28ff1b26ca5ad
Mohamed-Amine Izem <mizem@c2r6p8.1337.ma>
2024-08-18 13:46:44 +0100

    f

--- utils/back_up_split.c
+++ utils/back_up_split.c
@@ -1,16 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipe_split.c                                       :+:      :+:    :+:   */
+/*   back_up_split.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mizem <mizem@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/20 12:29:36 by mizem             #+#    #+#             */
-/*   Updated: 2024/08/18 13:46:33 by mizem            ###   ########.fr       */
+/*   Updated: 2024/08/18 13:42:20 by mizem            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
 
 void	*ft_free(char **str)
 {
@@ -39,11 +40,17 @@ void	ft_strncpy(char *s1, char *s2, int len)
 	s1[i] = '\0';
 }
 
-int	mini_count(char *str, char c, int i, int flag)
+int	count_wc(char *str, char c)
 {
+	int	i;
 	int	wc;
+	int	flag;
 
+	i = 0;
 	wc = 0;
+	flag = 0;
+	if (!str)
+		return (0);
 	while (str[i])
 	{
 		if (str[i] == '"' || str[i] == 39)
@@ -67,43 +74,21 @@ int	mini_count(char *str, char c, int i, int flag)
 	return (wc);
 }
 
-int	count_wc(char *str, char c)
+char	**pipe_split(char *str, char c)
 {
-	int	i;
-	int	wc;
-	int	flag;
+	char	**out;
+	int		flag;
 
+	int i, j, start, end, wc;
+	flag = 0;
 	i = 0;
-	wc = 0;
-	flag = 0;
+	j = 0;
 	if (!str)
-		return (0);
-	wc = mini_count(str, c, i, flag);
-	return (wc);
-}
-
-int	allocate_out(char **out, char *str, int start, int end)
-{
-	if (start < end)
-	{
-		*out = malloc(sizeof(char) * ((end - start) + 1));
-		if (!*out)
-			ft_free(out);
-		ft_strncpy(*out, &str[start], (end - start));
-		return (1);
-	}
-	else
-		return (0);
-}
-
-void	mini_split(char **out, char *str, char c, int i)
-{
-	int	start;
-	int	end;
-	int	j;
-	int	flag;
-
-	flag = 0;
+		return (NULL);
+	wc = count_wc(str, c);
+	out = malloc(sizeof(char *) * (wc + 1));
+	if (!out)
+		return (NULL);
 	while (str[i])
 	{
 		while (str[i] && str[i] == c)
@@ -118,25 +103,15 @@ void	mini_split(char **out, char *str, char c, int i)
 			i++;
 		}
 		end = i;
-		if (allocate_out(out, str, start, end))
-			out++;
+		if (start < end)
+		{
+			out[j] = malloc(sizeof(char) * ((end - start) + 1));
+			if (!out[j])
+				ft_free(out);
+			ft_strncpy(out[j], &str[start], (end - start));
+			j++;
+		}
 	}
-	*out = NULL;
-}
-
-char	**pipe_split(char *str, char c)
-{
-	char	**out;
-	int		i;
-	int		wc;
-
-	i = 0;
-	if (!str)
-		return (NULL);
-	wc = count_wc(str, c);
-	out = malloc(sizeof(char *) * (wc + 1));
-	if (!out)
-		return (NULL);
-	mini_split(out, str, c, i);
+	out[j] = NULL;
 	return (out);
 }
