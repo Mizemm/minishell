4a9bddd195c404b4dfae8a3c2cb72070f8a018d8
mizem <155259820+Mizemm@users.noreply.github.com>
2024-10-17 12:09:43 +0100

    norm

---
+++ parsing/parsing_utils.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mizem <mizem@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/19 23:38:00 by mizem             #+#    #+#             */
+/*   Updated: 2024/10/16 13:49:49 by mizem            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	path_check(char *s)
+{
+	int		i;
+	char	str[5];
+
+	i = 0;
+	ft_strncpy(str, "PATH", 5);
+	while (s[i] && s[i] == str[i])
+		i++;
+	if (i == 4)
+		return (1);
+	return (0);
+}
+
+char	**environment(t_main *main)
+{
+	char	**tmp;
+
+	tmp = NULL;
+	while (main->env)
+	{
+		if (ft_strcmp(main->env->name, "PATH") == 0)
+		{
+			tmp = ft_split(main->env->value, ':');
+			return (tmp);
+		}
+		main->env = main->env->next;
+	}
+	return (tmp);
+}
+
+char	*return_path(char **ev, char *str)
+{
+	int		i;
+	char	*tmp;
+	char	*tmp2;
+	char	*slash;
+
+	i = 0;
+	slash = ft_strdup("/");
+	while (ev[i])
+	{
+		tmp2 = ft_strjoin(ev[i], slash);
+		tmp = ft_strjoin(tmp2, str);
+		free(tmp2);
+		if (access(tmp, X_OK) == 0)
+			return (free(slash), tmp);
+		free(tmp);
+		i++;
+	}
+	free(slash);
+	return (0);
+}
